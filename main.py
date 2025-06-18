05f6088f8cb743f0d300754912bcfef3b7e4d924
h0bbs99 <jdoakes13@gmail.com>
2025-06-18 18:50:41 -0500

    add verbose mode

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ else:
      raise ValueError("Please provide a prompt as a command-line argument.")
 
 from google import genai
+from google.genai import types
+
+messages = [
+    types.Content(
+         role="user", parts=[types.Part(text=ai_prompt)])]
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=ai_prompt)
+    model="gemini-2.0-flash-001", contents=messages)
 
 # Output the response from the API
 print(f"Response: {response.text}")
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+    print(f"User prompt: {ai_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
